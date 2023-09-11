6e8e213858b3175ac160545538cca5d5233880de
seoyeon-1206 <ssy_1206@naver.com>
2023-09-11 18:06:26 +0900

    inital_Setting

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by 신서연 on 2023/09/11.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
